b50f7f0bcdacda72c31e1894b7cc6e1d2ffc649b
Brittney B <brittneyb.dev@gmail.com>
2024-07-16 17:38:42 -0400

    Updated code - Task 5 - Get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,7 +58,21 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbnParam = req.params.isbn;
+
+  const reviews = books[isbnParam]["reviews"];
+
+  if (!reviews) {
+
+    res.status(404).json({ message: 'No reviews found for the ISBN provided' });
+
+  } else {
+
+    res.status(200).json({reviews});
+
+  }
+
 });
+  //return res.status(300).json({message: "Yet to be implemented"});
 
 module.exports.general = public_users;
