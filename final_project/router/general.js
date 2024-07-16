92ae7a218554cf29848bb5de0a2d300f3a008645
Brittney B <brittneyb.dev@gmail.com>
2024-07-16 16:40:27 -0400

    Updated code - Task 2 - get details ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,7 +20,9 @@ public_users.get('/books',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn],null,4));
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
