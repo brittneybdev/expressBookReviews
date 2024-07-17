80f077811277879c6d10613e1d5113af1ffe548b
Brittney B <brittneyb.dev@gmail.com>
2024-07-17 15:01:41 -0400

    Updated code - TAsk 8 - Review added

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -35,10 +35,6 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 }
         
 
-
-
-
-
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
@@ -71,8 +67,22 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn]
+    if (filtered_book) {
+        let review = req.query.review;
+        let reviewer = req.session.authorization['username'];
+        if(review) {
+            filtered_book['reviews'][reviewer] = review;
+            books[isbn] = filtered_book;
+        }
+        res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
+    }
+    else{
+        res.send("Unable to find this ISBN!");
+    }
+
 });
 
 module.exports.authenticated = regd_users;
